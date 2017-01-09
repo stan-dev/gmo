e61f9c7bc885df9c0157f885cc7df4a3926f4a40
Dustin Tran <dustinviettran@gmail.com>
2017-01-09 10:09:41 -0500

    add bernoulli glm experiment

--- R/est_covariance.R
+++ R/est_covariance.R
@@ -9,23 +9,24 @@
 #'
 #' @export
 est_covariance <- function(fit.gmo, ...) {
-  density_sims <- fit.gmo$.log_p(alpha_sims, m, g_flag=TRUE)
-  if (draws < 25) {
-    log_r <- density_sims$log_p - density_sims$log_g
-  } else {
-    log_r <- psislw(density_sims$log_p - density_sims$log_g)$lw_smooth
-  }
-  max_log_r <- max(log_r)
-  r <- exp(log_r - max_log_r)
+  #density_sims <- fit.gmo$.log_p(alpha_sims, m, g_flag=TRUE)
+  #if (draws < 25) {
+  #  log_r <- density_sims$log_p - density_sims$log_g
+  #} else {
+  #  log_r <- psislw(density_sims$log_p - density_sims$log_g)$lw_smooth
+  #}
+  #max_log_r <- max(log_r)
+  #r <- exp(log_r - max_log_r)
 
-  # Note that weighted.mean normalizes the importance ratios
-  log_p_phi <- max_log_r + log(mean(r))
-  grad_log_p_phi <- apply(density_sims$grad_log_p, 2, weighted.mean, r)
+  ## Note that weighted.mean normalizes the importance ratios
+  #log_p_phi <- max_log_r + log(mean(r))
+  #grad_log_p_phi <- apply(density_sims$grad_log_p, 2, weighted.mean, r)
 
-  grad_log_joint_sims
-  hess_log_joint_sims
-  log_p_phi <- mean()
-  grad_log_p_phi
-  hess_log_p_phi
-  return(diag(length(phi)))
+  #grad_log_joint_sims
+  #hess_log_joint_sims
+  #log_p_phi <- mean()
+  #grad_log_p_phi
+  #hess_log_p_phi
+  #return(diag(length(phi)))
+  return(matrix())
 }
---
+++ demo/models/bernoulli_glm.stan
@@ -0,0 +1,29 @@
+data {
+  int<lower=1> N;
+  int<lower=1> J;
+  int<lower=1> K;  // fixed as K = 2
+  int y[N];
+  int<lower=1, upper=J> group[N];
+}
+parameters {
+  vector[K] phi;              // mu, log_sigma_alpha
+  vector[J] alpha;
+}
+transformed parameters {
+  real mu;
+  real<lower=0> sigma_alpha;
+  mu = phi[1];
+  sigma_alpha = exp(phi[2]);
+}
+model {
+  real y_pred[N];
+
+  alpha ~ normal(0, sigma_alpha);
+  for (n in 1:N) {
+    y_pred[n] = mu + alpha[group[n]];
+  }
+  y ~ bernoulli_logit(y_pred);
+  // adjust for change of vars:
+  // log | d/d(log_sigma_alpha) exp(log_sigma_alpha) | = log_sigma_alpha
+  target += log(sigma_alpha);
+}
